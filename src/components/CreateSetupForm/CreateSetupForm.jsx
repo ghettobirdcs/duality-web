9e777c18a64eaef884e451369b2b33ffa851cea3
McKade Hinshaw <hinshaw.mckade@gmail.com>
2025-07-21 04:39:49 -0600

    tacmap uploads

--- src/components/CreateSetupForm/CreateSetupForm.jsx
+++ src/components/CreateSetupForm/CreateSetupForm.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import "./CreateSetupForm.css";
 
 import PlayerTabs from "../PlayerTabs/PlayerTabs.jsx";
