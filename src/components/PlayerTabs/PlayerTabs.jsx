110200bafccfced9aa600966f81a308f236c3259
McKade Hinshaw <hinshaw.mckade@gmail.com>
2025-07-21 19:50:56 -0600

    Hardcoded player names

--- src/components/PlayerTabs/PlayerTabs.jsx
+++ src/components/PlayerTabs/PlayerTabs.jsx
@@ -4,7 +4,7 @@ import "./PlayerTabs.css";
 const PlayerTabs = ({ selectedPlayer, onSelect }) => {
   return (
     <div className="player-picker">
-      {[1, 2, 3, 4, 5].map((num) => (
+      {["ghettobird", "IJustClick", "Zim", "Upstart", "Jello"].map((num) => (
         <div
           key={num}
           className={`player-tab${selectedPlayer === num ? " player-tab--active" : ""}`}
